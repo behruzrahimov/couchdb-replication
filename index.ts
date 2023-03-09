7e378ee0ace78fd300db450ca91dc71c95c72910
behruzrahimov <behruzrahimov580@gmail.com>
2023-03-09 11:26:04 +0500

    minor fix

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import Nano from "nano";
+const configBob = "http://bob:bob!@192.168.68.107:5984";
+const configAlice = "http://alice:alice!@192.168.68.107:5980";
 async function CouchDbExample() {
-  const nano = Nano("http://bob:bob!@192.168.68.103:5984");
-  const nano2 = Nano("http://alice:alice!@192.168.68.103:5980");
+  const nano = Nano(configBob);
+  const nano2 = Nano(configAlice);
   async function isDatabaseExist(name: string): Promise<boolean> {
     try {
       await nano.db.get(name);
@@ -20,7 +22,6 @@ async function CouchDbExample() {
       throw e;
     }
   }
-
   async function createReplicatorDataBase() {
     const dbName = "_replicator";
     const isExist1 = await isDatabaseExist(dbName);
@@ -32,56 +33,47 @@ async function CouchDbExample() {
       await nano2.db.create(dbName);
     }
   }
-
   async function asyncCall() {
     await createReplicatorDataBase();
     const dbName = "people";
     const isExist = await isDatabaseExist(dbName);
     if (isExist) await nano.db.destroy(dbName);
-
     await nano.db.create(dbName);
-
     const db = nano.db.use(dbName);
-
     interface iPerson extends Nano.MaybeDocument {
       _id: string;
       name: string;
       date: string;
     }
-
     class Person implements iPerson {
       _id: string;
       name: string;
       date: string;
-
       constructor(name: string, date: string) {
         this._id = "1";
         this.name = name;
         this.date = date;
       }
-
       processAPIResponse(response: Nano.DocumentInsertResponse) {
         if (response.ok) {
           this._id = response.id;
         }
       }
     }
-
     let person = new Person("JON", "2015-02-04");
     db.insert(person).then((response) => {
       person.processAPIResponse(response);
       console.log(person);
     });
-
     try {
       const isExist = await isReplicationExist({
-        source: `http://bob:bob!@192.168.68.103:5984/${db.config.db}`,
-        target: `http://alice:alice!@192.168.68.103:5980/${db.config.db}`,
+        source: `${configBob}/${db.config.db}`,
+        target: `${configAlice}/${db.config.db}`,
       });
       if (!isExist) {
         const response = await nano.db.replication.enable(
-          `http://bob:bob!@192.168.68.103:5984/${db.config.db}`,
-          `http://alice:alice!@192.168.68.103:5980/${db.config.db}`,
+          `${configBob}/${db.config.db}`,
+          `${configAlice}/${db.config.db}`,
           {
             create_target: true,
             continuous: false,
@@ -90,13 +82,13 @@ async function CouchDbExample() {
         console.log("response", response);
       }
       const isExist2 = await isReplicationExist2({
-        source: `http://alice:alice!@192.168.68.103:5980/${db.config.db}`,
-        target: `http://bob:bob!@192.168.68.103:5984/${db.config.db}`,
+        source: `${configAlice}/${db.config.db}`,
+        target: `${configBob}/${db.config.db}`,
       });
       if (!isExist2) {
         const response2 = await nano2.db.replication.enable(
-          `http://alice:alice!@192.168.68.103:5980/${db.config.db}`,
-          `http://bob:bob!@192.168.68.103:5984/${db.config.db}`,
+          `${configAlice}/${db.config.db}`,
+          `${configBob}/${db.config.db}`,
           {
             create_target: true,
             continuous: false,
@@ -107,7 +99,6 @@ async function CouchDbExample() {
     } catch (err) {
       console.error("REPLICATION", err);
     }
-
     async function isReplicationExist({
       source,
       target,
@@ -121,11 +112,8 @@ async function CouchDbExample() {
           target: target,
         },
       });
-
       return result.docs.length > 0;
     }
-    console.log("_active_tasks", await nano.db.get("_active_tasks"));
-
     async function isReplicationExist2({
       source,
       target,
@@ -139,13 +127,9 @@ async function CouchDbExample() {
           target: target,
         },
       });
-
       return result.docs.length > 0;
     }
-    console.log("_active_tasks2", await nano2.db.get("_active_tasks"));
   }
-
   await asyncCall();
 }
-
 await CouchDbExample();
